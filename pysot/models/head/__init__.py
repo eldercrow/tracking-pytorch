6478a57a25cbc0994f888820387ec16244645910
Hyunjoon Lee <eldercrow@gmail.com>
2019-12-31 19:49:03 +0900

    Difference not correlation.

--- pysot/models/head/__init__.py
+++ pysot/models/head/__init__.py
@@ -5,14 +5,12 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from pysot.models.head.rpn import UPChannelRPN, DepthwiseRPN, MultiRPN, MaxPoolRPN, MultiMaxPoolRPN
+from pysot.models.head.rpn import UPChannelRPN, DepthwiseRPN, MultiRPN
 
 RPNS = {
         'UPChannelRPN': UPChannelRPN,
         'DepthwiseRPN': DepthwiseRPN,
         'MultiRPN': MultiRPN,
-        'MultiMaxPoolRPN': MultiMaxPoolRPN,
-        'MaxPoolRPN': MaxPoolRPN,
        }
 
 
--- pysot/models/head/rpn.py
+++ pysot/models/head/rpn.py
@@ -88,63 +88,55 @@ class DepthwiseXCorr(nn.Module):
         return out
 
 
-class MaxPoolXCorr(nn.Module):
+class DepthwiseXDiff(nn.Module):
     '''
     '''
-    def __init__(self, in_channel, hiddens, out_channel, out_kernel, pool_size):
+    def __init__(self, in_channels, hidden, out_channels):
         #
-        super(MaxPoolXCorr, self).__init__()
+        super(DepthwiseXDiff, self).__init__()
         #
-        ich_all = [in_channel] + hiddens[:-1]
-        och_all = hiddens
-        conv_list = [self._build_one(ich, och) for (ich, och) in zip(ich_all, och_all)]
-        self.conv = nn.Sequential(*conv_list)
-
-        self.pool = nn.MaxPool2d(pool_size, stride=1)
-
-        self.gpool = nn.AdaptiveMaxPool2d(1)
-
-        self.head = nn.Conv2d(hiddens[-1], out_channel, kernel_size=out_kernel, padding=out_kernel//2)
-
-    def _build_one(self, ich, och):
-        return nn.Sequential(
-            nn.Conv2d(ich, och, 1, bias=False),
-            nn.BatchNorm2d(och),
-            nn.ReLU(inplace=True)
-        )
+        self.conv_kernel = nn.Sequential(
+                nn.Conv2d(in_channels, hidden, kernel_size=3, bias=False),
+                nn.BatchNorm2d(hidden),
+                nn.ReLU(inplace=False),
+                # nn.MaxPool2d(pool_size, stride=1),
+                )
+        self.conv_search = nn.Sequential(
+                nn.Conv2d(in_channels, hidden, kernel_size=3, bias=False),
+                nn.BatchNorm2d(hidden),
+                nn.ReLU(inplace=False),
+                # nn.MaxPool2d(pool_size, stride=1),
+                )
+        self.dwconv = nn.Conv2d(hidden, hidden, kernel_size=5, bias=False, groups=hidden)
+        self.head = nn.Sequential(
+                nn.BatchNorm2d(hidden),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(hidden, hidden, kernel_size=1, bias=False),
+                nn.BatchNorm2d(hidden),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(hidden, out_channels, kernel_size=1)
+                )
 
     def forward(self, kernel, search):
-        kernel = self.conv(kernel)
-        kernel = self.pool(kernel)
+        kernel = self.conv_kernel(kernel)
+        search = self.conv_search(search)
 
-        search = self.conv(search)
-        search = self.pool(search)
-        gs = self.gpool(search) + 1e-04
+        kernel = self.dwconv(kenrel)
+        search = self.dwconv(search)
 
-        feature = search * (kernel / gs).expand_as(search)
+        feature = search - kernel.expand_as(search)
+
         out = self.head(feature)
         return out
-
-
-class MaxPoolRPN(RPN):
-    def __init__(self, anchor_num=5, in_channel=256, hiddens=[256, 384, 512], pool_size=7):
-        super(MaxPoolRPN, self).__init__()
-        self.cls = MaxPoolXCorr(in_channel, hiddens, 2 * anchor_num, 1, pool_size)
-        self.loc = MaxPoolXCorr(in_channel, hiddens, 4 * anchor_num, 3, pool_size)
-
-    def forward(self, z_f, x_f):
-        cls = self.cls(z_f, x_f)
-        loc = self.loc(z_f, x_f)
-        return cls, loc
 
 
 class DepthwiseRPN(RPN):
     def __init__(self, anchor_num=5, in_channels=256, hiddens=256):
         super(DepthwiseRPN, self).__init__()
-        # self.cls = PSDWDiff(in_channels, ps_channels, out_channels, 2 * anchor_num)
-        # self.loc = PSDWDiff(in_channels, ps_channels, out_channels, 4 * anchor_num)
-        self.cls = DepthwiseXCorr(in_channels, hiddens, 2 * anchor_num)
-        self.loc = DepthwiseXCorr(in_channels, hiddens, 4 * anchor_num)
+        self.cls = DepthwiseXDiff(in_channels, hiddens, 2 * anchor_num)
+        self.loc = DepthwiseXDiff(in_channels, hiddens, 4 * anchor_num)
+        # self.cls = DepthwiseXCorr(in_channels, hiddens, 2 * anchor_num)
+        # self.loc = DepthwiseXCorr(in_channels, hiddens, 4 * anchor_num)
 
     def forward(self, z_f, x_f):
         cls = self.cls(z_f, x_f)
@@ -159,47 +151,6 @@ class MultiRPN(RPN):
         for i in range(len(in_channels)):
             self.add_module('rpn'+str(i+2),
                     DepthwiseRPN(anchor_num, in_channels[i], hiddens[i]))
-        if self.weighted:
-            self.cls_weight = nn.Parameter(torch.ones(len(in_channels)))
-            self.loc_weight = nn.Parameter(torch.ones(len(in_channels)))
-
-    def forward(self, z_fs, x_fs):
-        cls = []
-        loc = []
-        # cls_head = []
-        for idx, (z_f, x_f) in enumerate(zip(z_fs, x_fs), start=2):
-            rpn = getattr(self, 'rpn'+str(idx))
-            c, l = rpn(z_f, x_f)
-            cls.append(c)
-            loc.append(l)
-
-        if self.weighted:
-            cls_weight = F.softmax(self.cls_weight, 0)
-            # loc_weight = [0, 0, 1]
-            loc_weight = F.softmax(self.loc_weight, 0)
-
-        def avg(lst):
-            return sum(lst) / len(lst)
-
-        def weighted_avg(lst, weight):
-            s = 0
-            for i in range(len(weight)):
-                s += lst[i] * weight[i]
-            return s
-
-        if self.weighted:
-            return weighted_avg(cls, cls_weight), weighted_avg(loc, loc_weight)
-        else:
-            return avg(cls), avg(loc)
-
-
-class MultiMaxPoolRPN(RPN):
-    def __init__(self, anchor_num, in_channels, hiddens, pool_size=7, weighted=False):
-        super(MultiMaxPoolRPN, self).__init__()
-        self.weighted = weighted
-        for i in range(len(in_channels)):
-            self.add_module('rpn'+str(i+2),
-                    MaxPoolRPN(anchor_num, in_channels[i], hiddens, pool_size))
         if self.weighted:
             self.cls_weight = nn.Parameter(torch.ones(len(in_channels)))
             self.loc_weight = nn.Parameter(torch.ones(len(in_channels)))
